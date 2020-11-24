704b41129016898d61b10162cb74017014366d70
beibei28845 <1097109986@qq.com>
2020-11-24 10:33:15 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,17 +8,6 @@ use Auth;
 
 class SessionsController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('guest', [
-            'only' => ['create']
-        ]);
-    }
-
-
-
-
     public function create()
     {
         return view('sessions.create');
@@ -31,10 +20,9 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials, $request->has('remember'))) {
+       if (Auth::attempt($credentials)) {
            session()->flash('success', '欢迎回来！');
-           $fallback = route('users.show', Auth::user());
-           return redirect()->intended($fallback);
+           return redirect()->route('users.show', [Auth::user()]);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
