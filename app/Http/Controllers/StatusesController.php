578b4790c96de1496013dbd5c357eed65c6a8bb6
beibei28845 <1097109986@qq.com>
2020-12-04 14:53:08 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StatusesController extends Controller
 {
@@ -10,15 +12,10 @@ class StatusesController extends Controller
     {
         $this->middleware('auth');
     }
-    class StatusesController extends Controller
-    {
-        public function __construct()
-        {
-            $this->middleware('auth');
-        }
 
-        public function store(Request $request)
-        {
+
+    public function store(Request $request)
+    {
             $this->validate($request, [
                 'content' => 'required|max:140'
             ]);
@@ -28,6 +25,14 @@ class StatusesController extends Controller
             ]);
             session()->flash('success', '发布成功！');
             return redirect()->back();
-        }
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
+    }
 
 }
